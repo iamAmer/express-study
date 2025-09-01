47722d9a6a4d824a210856af4325a74fc78eb95c
amer <muhammedamer7810@gmail.com>
2025-09-01 17:23:23 +0300

    Add cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,19 @@
 import express from "express";
 import routes from './routes/index.mjs'
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(routes);
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
+    // this route that sets the cookies
+    // you must visit first in order to authenticate & set it in the server side
+    response.cookie('hello', 'world', {maxAge: 10000})
     response.status(201).send('Hello World');
 })
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,10 +2,14 @@ import { Router } from 'express'
 const router = Router();
 
 router.get('/api/products', (request, response) => {
-    response.send([
-        {id: 345, name: "phone"},
-        {id: 346, name: "ipad"},
-    ]);
+    console.log(request.headers.cookie);
+    console.log(request.cookies)
+
+    // How authentication and authorization work
+    if(request.cookies.hello && request.cookies.hello == "world")
+        return response.send([ {id: 345, name: "phone"} ]);
+
+    return response.send({msg: "Sorry, you need the correct cookie!"})
 })
 
 export default router;
